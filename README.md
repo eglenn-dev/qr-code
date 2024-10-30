89ee96fea15071f8a2afc8fefeefd175df014e39
Ethan Glenn <ethan+git@eglenn.dev>
2024-10-30 16:52:31 -0600

    Functioning beta version

--- README.md
+++ README.md
@@ -1,36 +1,26 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+## About Project
+
+This project was created as a SPA that can help users create QR codes easily.
+
+I made it becuase I was tired of bloated webstes that were slow and had too many ads. I wanted to make a simple and easy to use QR code generator that was fast and efficient. I hope you enjoy using it!
+
+## Features
+
+-   Customize the colors
+-   Customize the size
+-   Custome URL that is actually encoded in the QR code, and not a redirect link to the destination website.
+-   Download the QR code as a PNG
+
+## Development
+
+Feel free to fork this project and make it your own! I would love to see what you can do with it.
 
 ## Getting Started
 
-First, run the development server:
+1.  Clone the repo
+2.  Run `npm install`
+3.  Run `npm run dev`
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## Deployment
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+This website is currently deployed on [Vercel](https://vercel.com/).
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,104 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import QRCode from "qrcode";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [qrCodeUrl, setQrCodeUrl] = useState("https://qr.eglenn.app");
+  const [outputUrl, setOutputUrl] = useState("");
+  const [light, setLight] = useState("#ffffff");
+  const [dark, setDark] = useState("#000000");
+  const [size, setSize] = useState(300);
+
+  useEffect(() => {
+    QRCode.toDataURL(qrCodeUrl, { width: size, color: { dark: dark, light: light } }, function (err: Error | null | undefined, url: string) {
+      if (err) {
+        console.log("Error generating QR code", err);
+        return;
+      }
+      setOutputUrl(url);
+    })
+  }, [qrCodeUrl, size, light, dark]);
+
+  return (
+    <main className={styles.main}>
+      <h1 className="gradient">QR Code Generator</h1>
+      <section className={styles.grid}>
+        <div className={styles.form}>
+          <span className="gradient-2">Settings</span>
+          <div className="coolinput">
+            <label htmlFor="size" className="text">Size (px)</label>
+            <input
+              type="text"
+              placeholder="Enter size in pixels"
+              name="size"
+              className="input"
+              defaultValue={300}
+              onChange={(e) => {
+                if (!parseInt(e.target.value)) {
+                  setSize(300);
+                  return;
+                }
+                if (parseInt(e.target.value) > 1000) {
+                  e.target.value = "1000";
+                  setSize(1000);
+                } else if (parseInt(e.target.value) < 50 || e.target.value === "") {
+                  setSize(50);
+                } else {
+                  setSize(parseInt(e.target.value));
+                }
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="coolinput">
+            <label htmlFor="url" className="text">URL</label>
+            <input
+              type="url"
+              placeholder="Enter valid url"
+              name="url"
+              className="input"
+              defaultValue="https://qr.eglenn.app"
+              onChange={(e) => {
+                if (e.target.value.includes("http://") || e.target.value.includes("https://")) {
+                  setQrCodeUrl(e.target.value);
+                } else {
+                  setQrCodeUrl("https://" + e.target.value);
+                }
+              }}
+            />
+          </div>
+          <div className="coolinput">
+            <label htmlFor="light" className="text">BG</label>
+            <input
+              type="color"
+              name="light"
+              className="input"
+              defaultValue="#ffffff"
+              onChange={(e) => setLight(e.target.value)}
+            />
+          </div>
+          <div className="coolinput">
+            <label htmlFor="dark" className="text">Code</label>
+            <input
+              type="color"
+              name="dark"
+              className="input"
+              defaultValue="#000000"
+              onChange={(e) => setDark(e.target.value)}
+            />
+          </div>
+          {outputUrl && <a href={outputUrl} download="qr-code.png" className={styles.downloadButton}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11zm-6 4q-.825 0-1.412-.587T4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413T18 20z"></path></svg>
+          </a>}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className={styles.imageArea}>
+          <div style={{ height: (size + 50), width: (size + 50), maxWidth: "300px", maxHeight: "300px" }} className={styles.imageOutput}>
+            {outputUrl && size && <Image src={outputUrl} alt="QR Code" width={size} height={size} />}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
